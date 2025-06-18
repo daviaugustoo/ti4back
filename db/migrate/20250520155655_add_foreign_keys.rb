10b2f627f4512e1e0de3dd2974ec540b81eb0ea7
Davi Augusto <113954562+daviaugustoo@users.noreply.github.com>
2025-06-18 17:52:33 -0300

    Corrige a coluna da chave estrangeira na tabela 'residuos' de 'evento_Id' para 'evento_id'.

--- db/migrate/20250520155655_add_foreign_keys.rb
+++ db/migrate/20250520155655_add_foreign_keys.rb
@@ -3,6 +3,6 @@ class AddForeignKeys < ActiveRecord::Migration[7.1]
     rename_column :eventos, :eventoId, :empresa_id
     rename_column :residuos, :eventoId, :evento_id
     add_foreign_key :eventos, :empresas, column: :empresa_id, primary_key: :id
-    add_foreign_key :residuos, :eventos, column: :evento_Id, primary_key: :id
+    add_foreign_key :residuos, :eventos, column: :evento_id, primary_key: :id
   end
 end
